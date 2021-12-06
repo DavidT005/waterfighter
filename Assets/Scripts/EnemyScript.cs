d7af5511e1fd9a37e3cb273e6b7b5dabaa7d84e5
David Tobon <davidtobon.dat@gmail.com>
2021-12-06 10:21:33 -0500

    Updated enemy AI so it has limited view distance and stops at shooting distance

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -7,6 +7,8 @@ public class EnemyScript : MonoBehaviour
 {
 
     public float health = 50f;
+    public float viewDistance = 30f;
+    public float attackDistance = 5f;
 
     public GameObject enemyDestination;
     NavMeshAgent theAgent;
@@ -18,7 +20,7 @@ public class EnemyScript : MonoBehaviour
 
     void Update()
     {
-        theAgent.SetDestination(enemyDestination.transform.position);
+        Move();
     }
 
     public void TakeDamage(float ammout){
@@ -31,5 +33,51 @@ public class EnemyScript : MonoBehaviour
     void Die(){
         Destroy(gameObject);
     }
+
+    void Move()
+    {
+        float distanceToPlayer = (enemyDestination.transform.position - transform.position).magnitude;
+        if (distanceToPlayer <= 20)
+        {
+            if (distanceToPlayer >= 10) theAgent.SetDestination(transform.position);
+            //TODO: attack logic
+            transform.LookAt(enemyDestination.transform.position);
+        }
+                    
+        if (distanceToPlayer <= 30 && distanceToPlayer > 20)
+        {
+            theAgent.SetDestination(enemyDestination.transform.position);
+        }
+
+        if (distanceToPlayer > 30)
+        {
+            return;
+        }
+
+
+
+
+
+        //theAgent.SetDestination(enemyDestination.transform.position);
+
+        /*
+        float distanceToDestination = Mathf.Abs((enemyDestination.transform.position - transform.position).magnitude);
+        if (distanceToDestination <= viewDistance && distanceToDestination >= attackDistance)
+        {
+            theAgent.SetDestination(enemyDestination.transform.position);
+        }
+        if (distanceToDestination <= viewDistance && distanceToDestination <= attackDistance)
+        {
+            theAgent.SetDestination(transform.position);    //Stays on current position
+            //TODO: attack logic
+        }
+        else
+        {
+            theAgent.SetDestination(transform.position);
+        }
+        */
+    }
+
+
 
 }
